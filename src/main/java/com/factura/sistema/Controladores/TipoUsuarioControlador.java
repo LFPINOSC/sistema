70002e3c5af7267aa6327873995e49b4507a331d
LFPINOSC <66181560+LFPINOSC@users.noreply.github.com>
2025-09-29 18:41:26 -0500

    cambio libreria

--- src/main/java/com/factura/sistema/Controladores/TipoUsuarioControlador.java
+++ src/main/java/com/factura/sistema/Controladores/TipoUsuarioControlador.java
@@ -1,7 +1,10 @@
 package com.factura.sistema.Controladores;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,11 +35,11 @@ public class TipoUsuarioControlador {
     }
     @GetMapping
     private List<TipoUsuario> listar(){
-        return tipoUsuarioServicio.listaAll();
+        return tipoUsuarioServicio.ListaAll();
     }
     @GetMapping("/{id}")
     private TipoUsuario obtener(@PathVariable Long id){
-       return tipoUsuarioServicio.listaTipoUsuario(id);
+       return tipoUsuarioServicio.ListaTipoUsuario(id);
     }
     
     @DeleteMapping("/{id}")
