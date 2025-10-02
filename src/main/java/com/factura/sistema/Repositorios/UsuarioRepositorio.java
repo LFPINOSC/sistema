648005d4fc7ad3b210a3ad79a3e76e0ffe17edb8
LFPINOSC <66181560+LFPINOSC@users.noreply.github.com>
2025-10-02 14:00:14 -0500

    cambios de seguridad

--- src/main/java/com/factura/sistema/Repositorios/UsuarioRepositorio.java
+++ src/main/java/com/factura/sistema/Repositorios/UsuarioRepositorio.java
@@ -1,10 +1,12 @@
 package com.factura.sistema.Repositorios;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.factura.sistema.Entidades.Usuario;
 
 public interface UsuarioRepositorio extends JpaRepository<Usuario,Long>{
-
-    
+    Optional<Usuario> findByUsername(String username);
+    boolean existsByUsername(String username);
 } 
---
+++ src/main/java/com/factura/sistema/Servcios/CustomUserDetailsService.java
@@ -0,0 +1,47 @@
+package com.factura.sistema.Servcios;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.factura.sistema.Entidades.Usuario;
+import com.factura.sistema.Repositorios.UsuarioRepositorio;
+
+;
+
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepositorio usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println("Username extraído del token: " + username);
+        Optional<Usuario> usuarioOpt = usuarioRepository.findByUsername(username);
+
+        Usuario usuario = usuarioOpt.orElseThrow(() -> 
+            new UsernameNotFoundException("Usuario no encontrado con username: " + username));
+
+        // Aquí debes convertir tu entidad Usuario a UserDetails
+        return new org.springframework.security.core.userdetails.User(
+            usuario.getUsername(), 
+            usuario.getPassword(), 
+            mapearRoles(usuario.getTipoUsuario().getNombre()) // Método para convertir roles a GrantedAuthority
+        );
+    }
+
+    private Collection<? extends GrantedAuthority> mapearRoles(String nombreRol) {
+        return List.of(new SimpleGrantedAuthority(nombreRol));
+    }
+}
