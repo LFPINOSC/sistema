480a8cb692f84e25aa4fb58d57e12fc7a7336735
LFPINOSC <66181560+LFPINOSC@users.noreply.github.com>
2025-10-02 17:16:40 -0500

    guardar

--- src/main/java/com/factura/sistema/Security/SecurityConfig.java
+++ src/main/java/com/factura/sistema/Security/SecurityConfig.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -55,5 +57,8 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-        
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }    
 }
